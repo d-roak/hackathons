06d625e50e507a380081cd84d8d2ce489c9d7664
Pedro <pedro.validex@gmail.com>
2022-05-07 16:10:16 +0100

    added basic functionalities

---
+++ 2022-HackMoney/contracts/CopyCat.sol
@@ -0,0 +1,48 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract CopyCat {
+    uint256 private nCopycats;
+    mapping(address => uint256) private balances;
+    mapping(address => address) private walletsToBeCopied;
+
+    //owner to send fees to
+    address private owner;
+
+    constructor() payable {
+        owner = msg.sender;
+        nCopycats = 0;
+    }
+
+    function deposit() public payable returns (uint256) {
+        balances[msg.sender] += msg.value;
+        return balances[msg.sender];
+    }
+
+    function withdraw(uint256 amount) public returns (uint256 newBalance) {
+        require(amount <= balances[msg.sender], "Amount exceeds balance");
+        balances[msg.sender] -= amount;
+        msg.sender.transfer(amount);
+        return balances[msg.sender];
+    }
+
+    function balance() public view returns (uint256) {
+        return balances[msg.sender];
+    }
+
+    function addWalletToCopyCat(address wallet) public {
+        //check if client is already copying another person
+        //maybe add feature to let one user deposit different amounts to copycat different wallets
+        walletsToBeCopied[msg.sender] = wallet;
+    }
+
+    function openPosition(address smartContractAddress, uint256 amount) public {
+
+    }
+
+    function closePosition(address smartContractAddress, uint amount) public {
+        
+    }
+}
