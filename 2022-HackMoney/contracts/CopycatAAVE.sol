b983f800500c33873d498afa23b1b19aa6374fe0
João Carvalho <5263301+jpldcarvalho@users.noreply.github.com>
2022-05-13 23:38:55 +0100

    add uniswap and aave contracts base

--- 2022-HackMoney/contracts/CopycatAAVE.sol
+++ 2022-HackMoney/contracts/CopycatAAVE.sol
@@ -5,30 +5,31 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
 
 contract CopycatAAVE {
+	IPool pool;
 
-    constructor() {}
+	constructor() {}
 
-    function deposit() public {
+	function deposit() public {
 
-    }
+	}
 
-    function setUserUseReserveAsCollateral() public {
+	function setUserUseReserveAsCollateral() public {
 
-    }
+	}
 
-    function borrow() public {
+	function borrow() public {
 
-    }
+	}
 
-    function repay() public {
+	function repay() public {
 
-    }
+	}
 
-    function swapBorrowRateModel() public {
+	function swapBorrowRateModel() public {
 
-    }
+	}
 
-    function rebalanceStableBorrowRate() public {
+	function rebalanceStableBorrowRate() public {
 
-    }
+	}
 }
---
+++ 2022-HackMoney/contracts/CopycatUniswap.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity >0.8.0;
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+
+contract CopycatUniswap {
+  IUniswapV3Pool pool;
+
+  function doSomethingWithPool() public {
+
+  }
+}
