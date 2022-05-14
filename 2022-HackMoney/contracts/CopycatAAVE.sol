d6e029aae782775f6f2e4c7cdb4558b6ca6765da
João Carvalho <5263301+jpldcarvalho@users.noreply.github.com>
2022-05-14 21:54:39 +0100

    copycataave update

--- 2022-HackMoney/contracts/CopycatAAVE.sol
+++ 2022-HackMoney/contracts/CopycatAAVE.sol
@@ -5,86 +5,82 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
 import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
 import {IUiPoolDataProviderV3} from '@aave/periphery-v3/contracts/misc/interfaces/IUiPoolDataProviderV3.sol';
+import {WalletBalanceProvider} from "@aave/periphery-v3/contracts/misc/WalletBalanceProvider.sol";
 
 contract CopycatAAVE {
-    IPool pool;
-		IUiPoolDataProviderV3 poolDataProvider;
-		mapping(address => mapping(address => mapping(address => IUiPoolDataProviderV3.UserReserveData))) private prevUserReserveData;
+	IPool pool;
+	IUiPoolDataProviderV3 poolDataProvider;
+	WalletBalanceProvider walletBalanceProvider;
+	mapping(address => mapping(address => IUiPoolDataProviderV3.UserReserveData)) private prevUserReserves;
 
-    constructor() {}
+	constructor() {}
 
-    function update(address copycat, address wallet, address token, uint256 amount)
-        public
-        view
-        returns (bool)
-    {
-			IPoolAddressesProvider poolProvider = pool.ADDRESSES_PROVIDER();
-			IUiPoolDataProviderV3.UserReserveData[] memory userReserveData;
-			// no idea what is the second return value
-			uint256 u;
-			(userReserveData, u) = poolDataProvider.getUserReservesData(poolProvider, wallet);
+	function update(address copied, address token, uint256 available)
+			public
+			payable
+			returns (bool)
+	{
+		IPoolAddressesProvider poolProvider = pool.ADDRESSES_PROVIDER();
+		IUiPoolDataProviderV3.UserReserveData[] memory userReserveData;
+		// no idea what is the second return value
+		uint256 u;
+		(userReserveData, u) = poolDataProvider.getUserReservesData(poolProvider, copied);
 
-			if (userReserveData.length == 0) {
-				return false;
-			}
-
-			IUiPoolDataProviderV3.UserReserveData prevUserReserveData = prevTokenBalance[copycat][wallet][token];
-			IUiPoolDataProviderV3.UserReserveData newUserReserveData;
-			for (uint256 i = 0; i < userReserveData.length; i++) {
-				if (userReserveData[i].underlyingAsset.toString() == token.toString()) {
-					newUserReserveData = userReserveData[i];
-					break;
-				}
-			}
-
-
+		if (userReserveData.length == 0) {
+			return false;
 		}
 
-    function withdraw(
-        address asset,
-        uint256 amount,
-        address to
-    ) public {
-        pool.withdraw(asset, amount, to);
-    }
+		IUiPoolDataProviderV3.UserReserveData memory prevUserReserveData = prevUserReserves[copied][token];
+		IUiPoolDataProviderV3.UserReserveData memory newUserReserveData;
+		for (uint256 i = 0; i < userReserveData.length; i++) {
+			if (userReserveData[i].underlyingAsset == token) {
+				newUserReserveData = userReserveData[i];
+				break;
+			}
+		}
 
-    function borrow(
-        address asset,
-        uint256 amount,
-        uint256 interestRateMode,
-        uint16 referralCode,
-        address onBehalfOf
-    ) public {
-        pool.borrow(asset, amount, interestRateMode, referralCode, onBehalfOf);
-    }
+		bool eqScaledAToken = prevUserReserveData.scaledATokenBalance == newUserReserveData.scaledATokenBalance;
+		bool eqCollateralEnabled = prevUserReserveData.usageAsCollateralEnabledOnUser == newUserReserveData.usageAsCollateralEnabledOnUser;
+		bool eqVariableDebt = prevUserReserveData.scaledVariableDebt == newUserReserveData.scaledVariableDebt;
+		bool eqStableDebt = prevUserReserveData.principalStableDebt == newUserReserveData.principalStableDebt;
+		
+		if(eqScaledAToken && eqCollateralEnabled && eqVariableDebt && eqStableDebt) {
+			return false;
+		}
 
-    function repay(
-        address asset,
-        uint256 amount,
-        uint256 rateMode,
-        address onBehalfOf
-    ) public {
-        pool.repay(asset, amount, rateMode, onBehalfOf);
-    }
+		// TODO calculate amounts
+		uint256 totalCopycat = available + 1;
+		uint256 totalCopied = walletBalanceProvider.balanceOf(copied, token);
+		// + newUserReserveData.scaledATokenBalance;
 
-    function repayWithATokens(
-        address asset,
-        uint256 amount,
-        uint256 interestRateMode
-    ) public {
-        pool.repayWithATokens(asset, amount, interestRateMode);
-    }
+		if(!eqScaledAToken) {
+			if (prevUserReserveData.scaledATokenBalance > newUserReserveData.scaledATokenBalance) {
+				pool.withdraw(token, available, msg.sender);
+			} else {
+				pool.supply(token, available, msg.sender, 0);
+			}
+		}
+		if(!eqCollateralEnabled) {
+			pool.setUserUseReserveAsCollateral(token, newUserReserveData.usageAsCollateralEnabledOnUser);
+		}
 
-    function swapBorrowRateMode(address asset, uint256 rateMode) public {
-        pool.swapBorrowRateMode(asset, rateMode);
-    }
+		// Stable: 1, Variable: 2
+		if(!eqVariableDebt) {
+			if (prevUserReserveData.scaledVariableDebt > newUserReserveData.scaledVariableDebt) {
+				pool.repay(token, available, 2, msg.sender);
+			} else {
+				pool.borrow(token, available, 2, 0, msg.sender);
+			}
+		}
+		if(!eqStableDebt) {
+			if (prevUserReserveData.principalStableDebt > newUserReserveData.principalStableDebt) {
+				pool.repay(token, available, 1, msg.sender);
+			} else {
+				pool.borrow(token, available, 1, 0, msg.sender);
+			}
+		}
 
-    function rebalanceStableBorrowRate(address asset, address user) public {
-        pool.rebalanceStableBorrowRate(asset, user);
-    }
-
-    function setUserUseReserveAsCollateral(address asset, bool useAsCollateral) public
-    {
-        pool.setUserUseReserveAsCollateral(asset, useAsCollateral);
-    }
+		prevUserReserves[copied][token] = newUserReserveData;
+		return true;
+	}
 }
