341ecb7936affa2ee1c461f952a3c5780aeb6c0b
Pedro <pedro.validex@gmail.com>
2022-05-14 19:10:59 +0100

    deploy and run scripts

---
+++ 2022-HackMoney/scripts/deploy.js
@@ -0,0 +1,18 @@
+const main = async () => {
+    const contractFactory = await hre.ethers.getContractFactory('Copycat');
+    const contract = await contractFactory.deploy();
+    await contract.deployed();
+    console.log("Copycat deployed to %s", contract.address);
+
+}
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0)
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+
+}
