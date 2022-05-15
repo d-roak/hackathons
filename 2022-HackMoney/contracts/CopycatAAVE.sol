06f4bda4218d360ee98fc5e36c57fcda5622b2af
João Carvalho <5263301+jpldcarvalho@users.noreply.github.com>
2022-05-15 15:52:22 +0100

    added stable and variable borrow formulas

--- 2022-HackMoney/contracts/CopycatAAVE.sol
+++ 2022-HackMoney/contracts/CopycatAAVE.sol
@@ -4,6 +4,7 @@ pragma solidity >0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
 import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IAaveOracle} from "@aave/core-v3/contracts/interfaces/IAaveOracle.sol";
 import {IUiPoolDataProviderV3} from "@aave/periphery-v3/contracts/misc/interfaces/IUiPoolDataProviderV3.sol";
 import {WalletBalanceProvider} from "@aave/periphery-v3/contracts/misc/WalletBalanceProvider.sol";
 
@@ -15,7 +16,13 @@ contract CopycatAAVE {
     mapping(address => mapping(address => IUiPoolDataProviderV3.UserReserveData))
         private prevUserReserves;
 
-    function addWallet(address wallet){
+		constructor() public {
+			pool = IPool(0x794a61358D6845594F94dc1DB02A252b5b4814aD);
+			poolDataProvider = IUiPoolDataProviderV3(0x69FA688f1Dc47d4B5d8029D5a35FB7a548310654);
+			walletBalanceProvider = WalletBalanceProvider(0xBc790382B3686abffE4be14A030A96aC6154023a);
+		}
+
+    function addWallet(address wallet) public {
         
     }
 
@@ -71,8 +78,7 @@ contract CopycatAAVE {
     ) public payable returns (bool) {
         IUiPoolDataProviderV3.UserReserveData[] memory userReserveData;
         // no idea what is the second return value
-        uint256 u;
-        (userReserveData, u) = poolDataProvider.getUserReservesData(
+        (userReserveData, uint256) = poolDataProvider.getUserReservesData(
             pool.ADDRESSES_PROVIDER(),
             copied
         );
@@ -92,7 +98,7 @@ contract CopycatAAVE {
         }
 
         IUiPoolDataProviderV3.UserReserveData memory walletReserveData;
-        (userReserveData, u) = poolDataProvider.getUserReservesData(
+        (userReserveData, uint256) = poolDataProvider.getUserReservesData(
             pool.ADDRESSES_PROVIDER(),
             address(this)
         );
@@ -160,36 +166,131 @@ contract CopycatAAVE {
             );
         }
 
-        // TODO check with oracle
         // Stable: 1, Variable: 2
-        if (
-            prevUserReserveData.scaledVariableDebt !=
-            newUserReserveData.scaledVariableDebt
-        ) {
-            if (
-                prevUserReserveData.scaledVariableDebt >
-                newUserReserveData.scaledVariableDebt
-            ) {
-                pool.repay(token, balance, 2, address(this));
-            } else {
-                pool.borrow(token, balance, 2, 0, address(this));
-            }
-        }
-        if (
-            prevUserReserveData.principalStableDebt ==
-            newUserReserveData.principalStableDebt
-        ) {
-            if (
-                prevUserReserveData.principalStableDebt >
-                newUserReserveData.principalStableDebt
-            ) {
-                pool.repay(token, balance, 1, address(this));
-            } else {
-                pool.borrow(token, balance, 1, 0, address(this));
-            }
-        }
+				uint256 newPos = (getCurrentVariableBorrowPrice(address(this), token) - (getCurrentVariableBorrowPrice(copied, token) * getTotalCollateralPrice(address(this)) / getTotalCollateralPrice(copied)))
+					* getCurrentVariableBorrow(address(this), token)
+					/ getCurrentVariableBorrowPrice(address(this), token);
 
-        prevUserReserveData = newUserReserveData;
+				if (
+						walletReserveData.scaledVariableDebt >
+						newPos
+				) {
+						pool.repay(token, walletReserveData - newPos, 2, address(this));
+				} else if (
+						walletReserveData.scaledVariableDebt <
+						newPos
+				) {
+						pool.borrow(token, newPos - walletReserveData.scaledVariableDebt, 2, 0, address(this));
+				}
+
+				uint256 newPos = (getCurrentStableBorrowPrice(address(this), token) - (getCurrentStableBorrowPrice(copied, token) * getTotalCollateralPrice(address(this)) / getTotalCollateralPrice(copied)))
+					* getCurrentStableBorrow(address(this), token)
+					/ getCurrentStableBorrowPrice(address(this), token);
+
+				if (
+						walletReserveData.principalStableDebt >
+						newPos
+				) {
+						pool.repay(token, walletReserveData.principalStableDebt - newPos, 1, address(this));
+				} else if (
+						walletReserveData.principalStableDebt <
+						newPos
+				) {
+						pool.borrow(token, newPos - walletReserveData.principalStableDebt, 1, 0, address(this));
+				}
+
+				prevUserReserves[copied][token] = newUserReserveData;
         return true;
     }
+
+		function getTotalCollateralPrice(address addr) private view returns (uint256) {
+			IAaveOracle oracle = IAaveOracle(0xb023e699F5a33916Ea823A16485e259257cA8Bd1);
+
+			uint256 total = 0;
+			IUiPoolDataProviderV3.UserReserveData[] memory userReserveData;
+			// no idea what is the second return value
+			(userReserveData, uint256) = poolDataProvider.getUserReservesData(
+					pool.ADDRESSES_PROVIDER(),
+					addr
+			);
+
+			for (uint256 i = 0; i < userReserveData.length; i++) {
+					if (userReserveData[i].usageAsCollateralEnabledOnUser) {
+							total += oracle.getAssetPrice(userReserveData[i].underlyingAsset) * userReserveData[i].scaledATokenBalance;
+					}
+			}
+			return total;
+		}
+
+		function getCurrentStableBorrow(address addr,  address token) private view returns (uint256) {
+			IUiPoolDataProviderV3.UserReserveData[] memory userReserveData;
+			// no idea what is the second return value
+			(userReserveData, uint256) = poolDataProvider.getUserReservesData(
+					pool.ADDRESSES_PROVIDER(),
+					addr
+			);
+
+			for (uint256 i = 0; i < userReserveData.length; i++) {
+					if (userReserveData[i].usageAsCollateralEnabledOnUser) {
+						if (userReserveData[i].underlyingAsset == token) {
+							return userReserveData[i].principalStableDebt;
+						}
+					}
+			}
+			return 0;
+		}
+
+		function getCurrentStableBorrowPrice(address addr, address token) private view returns (uint256) {
+			IAaveOracle oracle = IAaveOracle(0xb023e699F5a33916Ea823A16485e259257cA8Bd1);
+
+			IUiPoolDataProviderV3.UserReserveData[] memory userReserveData;
+			// no idea what is the second return value
+			(userReserveData, uint256) = poolDataProvider.getUserReservesData(
+					pool.ADDRESSES_PROVIDER(),
+					addr
+			);
+
+			for (uint256 i = 0; i < userReserveData.length; i++) {
+					if (userReserveData[i].usageAsCollateralEnabledOnUser) {
+							return oracle.getAssetPrice(userReserveData[i].underlyingAsset) * userReserveData[i].principalStableDebt;
+					}
+			}
+			return 0;
+		}
+
+		function getCurrentVariableBorrow(address addr,  address token) private view returns (uint256) {
+			IUiPoolDataProviderV3.UserReserveData[] memory userReserveData;
+			// no idea what is the second return value
+			(userReserveData, uint256) = poolDataProvider.getUserReservesData(
+					pool.ADDRESSES_PROVIDER(),
+					addr
+			);
+
+			for (uint256 i = 0; i < userReserveData.length; i++) {
+					if (userReserveData[i].usageAsCollateralEnabledOnUser) {
+						if (userReserveData[i].underlyingAsset == token) {
+							return userReserveData[i].scaledVariableDebt;
+						}
+					}
+			}
+			return 0;
+		}
+
+		function getCurrentVariableBorrowPrice(address addr, address token) private view returns (uint256) {
+			IAaveOracle oracle = IAaveOracle(0xb023e699F5a33916Ea823A16485e259257cA8Bd1);
+
+			IUiPoolDataProviderV3.UserReserveData[] memory userReserveData;
+			// no idea what is the second return value
+			(userReserveData, uint256) = poolDataProvider.getUserReservesData(
+					pool.ADDRESSES_PROVIDER(),
+					addr
+			);
+
+			for (uint256 i = 0; i < userReserveData.length; i++) {
+					if (userReserveData[i].usageAsCollateralEnabledOnUser) {
+							return oracle.getAssetPrice(userReserveData[i].underlyingAsset) * userReserveData[i].scaledVariableDebt;
+					}
+			}
+			return 0;
+		}
 }
