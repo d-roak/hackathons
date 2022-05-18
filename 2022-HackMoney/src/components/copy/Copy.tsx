7392c99ca000a2269b92aee38a554b0605e393d9
João Carvalho <5263301+jpldcarvalho@users.noreply.github.com>
2022-05-18 07:49:08 +0100

    add copy form, remove copy modal and home component

--- 2022-HackMoney/src/components/copy/Copy.tsx
+++ 2022-HackMoney/src/components/copy/Copy.tsx
@@ -1,31 +1,40 @@
+import { useForm } from 'react-hook-form'
+
 
 function Copy() {
+	const {
+    handleSubmit,
+    register,
+    formState: { isSubmitting },
+  } = useForm()
+
+  async function onSubmit(values: any) {
+  }
+
 	return (
 		<div className="w-1/2 mt-10 mx-auto
-			text-gray-400 bg-gray-900 rounded-xl
+			text-gray-400 bg-gray-800 rounded-xl
 			p-6">
 			<h2 className="text-gray-300 text-center font-bold text-lg pb-3 mb-4 border-b border-gray-700">
 				Copy Address
 			</h2>
 			<div>
-				<p>
-					AAAAA
-				</p>
-				<p>
-					AAAAA
-				</p>
-				<p>
-					AAAAA
-				</p>
-				<p>
-					AAAAA
-				</p>
-				<p>
-					AAAAA
-				</p>
-				<p>
-					AAAAA
-				</p>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<div className="mb-6">
+						<label htmlFor="address" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Address</label>
+						<input type="text" id="address" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Wallet Address" required />
+					</div>
+					<div className="mb-6">
+						<label htmlFor="address" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Token</label>
+						<input type="text" id="address" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Token" required />
+					</div>
+					<div className="mb-6">
+						<label htmlFor="address" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Amount</label>
+						<input type="text" id="address" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Amount" required />
+					</div>
+  				
+					<button type="submit" className="text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+				</form>
 			</div>
 		</div>
 	);
